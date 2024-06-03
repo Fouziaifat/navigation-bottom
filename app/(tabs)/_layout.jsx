58e9852030b777dce8a66c096d0dbc7dfcad1fa6
Fouzia ifat <fouzia786ifat@gmail.com>
2024-06-03 06:29:18 +0530

    final commit

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,29 +1,30 @@
+import { Colors } from '@/constants/Colors';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'pink' }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: Colors.ICON_BG }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
           headerShown: false,
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={26} name="home" color={color} />,
         }}
       />
       <Tabs.Screen
         name="order"
         options={{
           title: 'Order',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="heart" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={26} name="heart" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Account',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user-o" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={26} name="user-o" color={color} />,
         }}
       />
     </Tabs >
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -23,4 +23,6 @@ export const Colors = {
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+  PRIMARY: '#FFD700',
+  ICON_BG: '#FF7F50'
 };
